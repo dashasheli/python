8ae67c63b79c761daeec8cc05c60851a181a7373
Daria <dashasheli@gmail.com>
2023-10-23 22:38:45 +0200

    ukol_04

--- test.py
+++ test.py
@@ -1,29 +1,26 @@
-def mobil():
+def get_mobile_number():
     cislo = input("Zadej cislo mobilu: +420")
     if cislo.isdigit():
         if len(cislo) == 9:
             return cislo
         else:
-            print("Chyba: potreba zadat 9 cislic")
+            print("Chyba: potřeba zadat 9 číslic")
             return None
     else:
-        print("Chyba: cislo mobilu musi byt cele cislo.")
+        print("Chyba: číslo mobilu musí být celé číslo.")
         return None
 
 def zprava(cislo):
     text = input("Zadejte text zpravy:")
-
     a = 180
     cena = 3
     pocet_sms = len(text) // a
-
     if len(text) % a != 0:
-         pocet_sms += 1
+        pocet_sms += 1
     konecna_cena = pocet_sms * cena
-        
-    print(f"Cena sms je {konecna_cena} KC")
+    print(f"Cena SMS je {konecna_cena} Kč")
 
-cislo = mobil()
+cislo = get_mobile_number()
 
 if cislo is not None:
     zprava(cislo)
---
+++ ukol_05_bonus.py
@@ -0,0 +1,11 @@
+teploty = [
+    ["pondělí", 2.1, 5.2, 6.1, -0.1],
+    ["úterý", 2.2, 4.8, 5.6, -1.0],
+    ["středa", 3.3, 6.5, 5.9, 1.2],
+    ["čtvrtek", 2.9, 5.6, 6.0, 0.0],
+    ["pátek", 2.0, 4.6, 5.5, -1.2],
+    ["sobota", 1.0, 3.2, 2.1, -2.0],
+    ["neděle", 0.4, 2.7, 1.3, -2.8]
+]
+
+# Pomocí dict comprehension vytvoř slovník, který bude mít následující formát, kde klíčem bude den v týdnu, a hodnotou průměrná teplota ten den.
